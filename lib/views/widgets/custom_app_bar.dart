fcfe71294f20dd7d832b6830b8a7cbeba9be4ff9
Ezalddin Naddaf <zldyn988@gmail.com>
2023-08-23 12:26:42 +0300

    create NotesListView

--- lib/views/widgets/custom_app_bar.dart
+++ lib/views/widgets/custom_app_bar.dart
@@ -8,12 +8,14 @@ class CustomAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Row(
+
       children: [
+        
         Text(
           'Notes',
           style: TextStyle(fontSize: 34),
         ),
-        SizedBox(width: 235,),
+        SizedBox(width: 198, ),
         CustomSearchIcon(),
       ],
     );
--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/views/widgets/notes_list_view.dart';
 
 import 'custom_app_bar.dart';
 
@@ -10,81 +10,14 @@ class NotesViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 70),
+      padding: EdgeInsets.only(top: 70, right: 40 , left: 32),
       child: Column(
         children: [
-          SizedBox(
-            height: 20,
-          ),
+
           CustomAppBar(),
-          SizedBox(
-            height: 15,
-          ),
-          NoteItem(),
+      Expanded(child: NotesListView()),
         ],
       ),
-    );
-  }
-}
-
-class NoteItem extends StatelessWidget {
-  const NoteItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(top: 30),
-          child: Container(
-            padding:const EdgeInsets.only(top: 12 , left: 16 , bottom: 12 , right: 8),
-            decoration: BoxDecoration(
-                color: const Color(0xffFFCC80),
-                borderRadius: BorderRadius.circular(16)),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                ListTile(
-                  
-                  contentPadding:const EdgeInsets.all(0),
-                  trailing: Padding(
-                    padding: const EdgeInsets.only(left: 20.0),
-                    child: IconButton(
-                      onPressed: () {},
-                      icon: Padding(
-                        padding: const EdgeInsets.only(left: 15 , bottom: 12),
-                        child: Icon(FontAwesomeIcons.trash,
-                            size: 25, color: Colors.black.withOpacity(.7)),
-                      ),
-                    ),
-                  ),
-                  title: const Padding(
-                    padding: EdgeInsets.only(top: 8.0, bottom: 12 ),
-                    child: Text(
-                      'Flutter tips',
-                      style: TextStyle(fontSize: 34, color: Colors.black),
-                    ),
-                  ),
-                  subtitle: Text(
-                    'How to make your presonal brand stand out  online',
-                    style: TextStyle(
-                        fontSize: 18,
-                        height: 1.4,
-                        color: Colors.black.withOpacity(.6)),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top:20.0),
-                  child: Text(
-                    'May 21/2023',
-                    style: TextStyle(color: Colors.black.withOpacity(.6) , fontSize: 15),
-                  ),
-                )
-              ],
-            ), 
-          ),
-        ),
-      ],
     );
   }
 }
